2535afd7f2d10940566085cc8ea54f76896bc05a
Oliver Kaufmann <okaufmann91@gmail.com>
2016-09-14 23:10:48 +0200

    Add auth and layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="panel panel-default">
+        <div class="panel-heading">Dashboard</div>
+
+        <div class="panel-body">
+            <p>
+                <a href="{{url('task/create')}}">Create a new task</a>
+            </p>
+
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>List</th>
+                        <th>Due to</th>
+                        <th>Created At</th>
+                        <th>Modified At</th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse($tasks as $task)
+                        <tr>
+                            <td>{{$task->id}}</td>
+                            <td>{{$task->name}}</td>
+                            <td>{{$task->list->name}}</td>
+                            <td>{{$task->due_date}}</td>
+                            <td>{{$task->created_at}}</td>
+                            <td>{{$task->modified_at}}</td>
+                            <td><a href="{{url('task/'.$task->id)}}" class="btn btn-primary">Edit</a></td>
+                            <td><a href="{{url('task/'.$task->id.'/delete')}}"
+                                   class="btn btn-danger">Delete</a></td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="8">No records</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,25 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', ['foo' => 'bar']);
 });
+
+Route::group(['middleware' => 'auth'], function (){
+    Route::get('/home', 'HomeController@index');
+
+    Route::get('task/create', 'TaskController@create');
+    Route::post('task/store', 'TaskController@store');
+    Route::get('task/{task}', 'TaskController@show');
+    Route::get('task/{task}', 'TaskController@edit');
+    Route::put('task', 'TaskController@update');
+    Route::delete('task/create', 'TaskController@destroy');
+
+
+});
+
+
+
+
+
+Auth::routes();
+
